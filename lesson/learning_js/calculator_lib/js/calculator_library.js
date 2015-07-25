5663b3157e2f271bca72769b886fe91c54f4865b
arnab003 <arnab.saha@fusioncharts.com>
2015-07-25 23:56:47 +0530

    moddify

--- lesson/learning_js/calculator_lib/js/calculator_library.js
+++ lesson/learning_js/calculator_lib/js/calculator_library.js
@@ -623,7 +623,7 @@
 						 hour+=24;
 						if(minute<0)
 						 minute+=60;
-						document.getElementById("r2").value=hour+":"+minute; 
+						document.getElementById("r2").value=hour+" hours "+minute+" minutes"; 
 					}  
 				}  
 				else
